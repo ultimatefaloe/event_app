8244ef1c2a1d142ccef5b53b054a8f330c4ebdaa
ultimatefaloe <ultimatefaloe@outlool.com>
2026-07-14 00:06:03 +0100

    feat: wire all routes (Home, EventList, CreateEvent, EventDetail, EditEvent, NotFound)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import RootLayout from "./components/layouts/RootLayout.jsx";
 import Home from "./pages/Home.jsx";
+import EventList from "./pages/EventList.jsx";
+import EventDetail from "./pages/EventDetail.jsx";
+import CreateEvent from "./pages/CreateEvent.jsx";
+import EditEvent from "./pages/EditEvent.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      {
-        index: true,
-        element: <Home />,
-      },
+      { index: true, element: <Home /> },
+      { path: "events", element: <EventList /> },
+      { path: "events/new", element: <CreateEvent /> },
+      { path: "events/:id", element: <EventDetail /> },
+      { path: "events/:id/edit", element: <EditEvent /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);
